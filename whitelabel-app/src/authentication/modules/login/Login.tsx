ca8cdd7821a90c5b96777c1fff2723da4d65267c
Bruno <amorimbq@gmail.com>
2022-11-26 16:24:06 -0300

    wip

---
+++ whitelabel-app/src/authentication/modules/login/Login.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {Button} from 'react-native-paper';
+export function Login() {
+  return (
+    <View>
+      <Text>Login</Text>
+      <Button onPress={() => {}} mode="contained" />
+    </View>
+  );
+}
